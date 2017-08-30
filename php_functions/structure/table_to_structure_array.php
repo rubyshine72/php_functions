22ffbdeb36fbb542563881b87545307a0aa6d928
rubyshine72 <rubyshine72@yahoo.com>
2017-08-30 20:21:46 +0800

    Update table_to_structure_array.php

--- php_functions/structure/table_to_structure_array.php
+++ php_functions/structure/table_to_structure_array.php
@@ -1,1 +1,23 @@
+<?php
 
+  public function getStructureProducts(){
+		
+		$return_array = array();
+		
+		$products = Product::where('enable', 1)
+						->orderBy('card_level', 'asc')
+						->get();
+    
+		foreach($products as $product){
+			$temp[] = array("data" => $product, "sub_list" => []);
+			$last_temp = end($temp);
+			if($product->parent == 0){
+				$temp_list[$product->id] = &$return_array;
+			}else{
+				$temp_list[$product->id] = &$temp_list[$product->parent][$product->parent]['list'];
+			}
+			$temp_list[$product->id][$product->id] = $last_temp;
+		}
+		
+		return $return_array;
+	}
